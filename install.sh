7321727bfe50e1ee3284c6f3a344eab7df9df252
Frederik Pietzko <frederik.pietzko@iits-consulting.de>
2022-11-23 15:07:46 +0100

    Install script for qtile symlink.

--- install.sh
+++ install.sh
@@ -2,10 +2,18 @@
 
 # Create Symlinkg for nvim
 echo "Creating Symlink for nvim config..."
-
-if test -f ~/.config/nvim; then
+if test -e ~/.config/nvim; then
+  echo "    Symlink already exists."
+else
   ln -s `pwd`/.config/nvim ~/.config/nvim || exit 1
   echo "    Symlink created at ~/.config/nvim!"
-else
+fi
+
+# Create Symlink for qtile
+echo "Creating Symlink for qtile config..."
+if test -e ~/.config/qtile; then
   echo "    Symlink already exists."
+else
+  ln -s `pwd`/.config/qtile ~/.config/qtile || exit 1
+  echo "    Symlink created at ~/.config/qtile"
 fi
