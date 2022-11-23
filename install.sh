68905091fcfbb62aa5a02dbfc8c290433af9f716
Frederik Pietzko <frederik.pietzko@iits-consulting.de>
2022-11-23 15:26:39 +0100

    Extract symlink creating to function.

--- install.sh
+++ install.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# Create Symlinkg for nvim
-echo "Creating Symlink for nvim config..."
-if test -e ~/.config/nvim; then
-  echo "    Symlink already exists."
-else
-  ln -s `pwd`/.config/nvim ~/.config/nvim || exit 1
-  echo "    Symlink created at ~/.config/nvim!"
-fi
 
-# Create Symlink for qtile
-echo "Creating Symlink for qtile config..."
-if test -e ~/.config/qtile; then
-  echo "    Symlink already exists."
-else
-  ln -s `pwd`/.config/qtile ~/.config/qtile || exit 1
-  echo "    Symlink created at ~/.config/qtile"
-fi
+createSymlink() {
+  # Args: Name, from, to
+  echo "Creating Symlink for $1"
+  if test -e $3; then
+    echo "   Symlink already exists."
+  else
+    ln -s "`pwd`/$2" "$3" || exit 1
+    echo "    Symlink created at $1"
+  fi
+}
+
+createSymlink "neovim" ".config/nvim" "$HOME/.config/nvim"
+createSymlink "qtile" ".config/qtile" "$HOME/.config/qtile"
+
